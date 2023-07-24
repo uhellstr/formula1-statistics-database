3e782dc142cf76e1ce746fffa04e1913616c8b0e
Ulf Hellström <oraminute@gmail.com>
2023-07-24 10:53:36 +0200

    Updated several sql files.

--- sql/install.sql
+++ sql/install.sql
@@ -4,6 +4,3 @@ REM Minimum version of APEX required is >= 5.0
 REM Run this script as SYS,SYSTEM or own DBA with full DBA permission to create schema and objects.
 REM 
 @setup_schema.sql
-@F1_LOGIK_SCHEDULER.sql
-@F1_ACL_SETUP.sql
-
--- sql/lb_install_f1_staging.sql
+++ sql/lb_install_f1_staging.sql
@@ -1,6 +1,10 @@
 REM
-REM Run this script connected as F1_DATA from the sql folder
+REM Run this script using SQLcmdline connected as F1_DATA from the sql folder
 REM
 cd ../F1_STAGING
 pwd
 lb update --changelog-file controller.xml
+cd ../sql
+pwd
+@insert_f1_json_doctype.sql
+@insert_f1_json_racetype.sql
