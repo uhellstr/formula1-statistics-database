5deccc45d9047066f64cf1b6daf88d10ce096dc7
Ulf Hellström <oraminute@gmail.com>
2023-09-21 11:06:06 +0200

    Updated F1_LOGIK grants to APEX_F1

--- F1_LOGIK/function/millis_to_laptime_function.sql
+++ F1_LOGIK/function/millis_to_laptime_function.sql
@@ -10,23 +10,23 @@
     lv_milliseconds number;
     lv_retval varchar2(15);
     lv_fmt_millis varchar2(6);
-    
+
 begin
   if p_in_milliseconds is not null then
-  
+
     -- calculate laptime components
     lv_hours := floor(p_in_milliseconds / 3600000);      -- divide by 3600000 to get hours
     lv_minutes := floor((p_in_milliseconds - (lv_hours * 3600000)) / 60000);   -- subtract hours and divide by 60000 to get minutes
     lv_seconds := floor((p_in_milliseconds - (lv_hours * 3600000) - (lv_minutes * 60000)) / 1000);   -- subtract hours and minutes and divide by 1000 to get seconds
     lv_milliseconds := p_in_milliseconds - (lv_hours * 3600000) - (lv_minutes * 60000) - (lv_seconds * 1000);   -- subtract hours, minutes, and seconds to get remaining milliseconds
-        
+
     lv_fmt_millis := case 
                      when lv_milliseconds between 0 and 99 then
                         rpad(lpad(to_char(lv_milliseconds),3,'0'),6,0)
                      else
                         rpad(to_char(lv_milliseconds), 6, '0')
                      end;
-                     
+
     -- format laptime into hh:mi:ss.ff
     lv_retval := lpad(to_char(lv_hours), 2, '0') || ':' ||
                  lpad(to_char(lv_minutes), 2, '0') || ':' ||
@@ -35,7 +35,7 @@ begin
   else
     lv_retval := null;
   end if;
-  
+
   return lv_retval;
-  
+
 end;
