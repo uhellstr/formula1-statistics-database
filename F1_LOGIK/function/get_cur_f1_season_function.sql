8c6073f454ebeb1e931b657e4c2e9ed0e0b264d1
Ulf Hellström <oraminute@gmail.com>
2024-01-04 12:12:21 +0100

    Updated get_cur_f1_season_function to handle in middle season

--- F1_LOGIK/function/get_cur_f1_season_function.sql
+++ F1_LOGIK/function/get_cur_f1_season_function.sql
@@ -1,5 +1,4 @@
-
-  CREATE OR REPLACE EDITIONABLE FUNCTION "F1_LOGIK"."GET_CUR_F1_SEASON" 
+CREATE OR REPLACE EDITIONABLE FUNCTION "F1_LOGIK"."GET_CUR_F1_SEASON" 
 (
   p_in_cur_year in varchar2 default to_char(current_date,'RRRR') 
 ) 
@@ -25,36 +24,45 @@ begin
       ) 
   select case
              when x.number_of_races - y.upcoming_races = x.number_of_races then 0
+             when x.number_of_races - y.upcoming_races = 0 then 0
              else 1
          end as races_done
   into lv_races
   from numb_races x
        ,upc_races y;
 
-  with future_races as -- We need to handle between seasons where there are no races
-    (
-      select /*+ MATERIALIZE */ count(vfu.season) as any_races
-      from f1_data.v_f1_upcoming_races vfu
-      where vfu.season = substr(to_char(trunc(sysdate,'YEAR')),1,4)  --Fix to YEAR and substr(1,4) to garantee that we only get the YEAR part
-    )
-    select season into lv_season -- Is current season finished yet?
-    from
-    (
-     select to_date(r.race_date,'RRRR-MM-DD') as race_date
-            ,case
-               when (r.race_date < trunc(sysdate) and x.any_races = 0 and lv_races = 0 and to_char(to_number(to_char(current_date,'RRRR'))) = p_in_cur_year) then to_char(to_number(to_char(current_date,'RRRR'))) -- season finished but same year 
-               when (r.race_date < trunc(sysdate) and x.any_races < 1) then to_char(to_number(to_char(current_date,'RRRR') - 1)) -- season finished but next years season has not yet started
-               when r.race_date < trunc(sysdate) then to_char(current_date,'RRRR')
-               when (r.race_date > trunc(sysdate) and x.any_races > 0) then to_char(current_date,'RRRR')
-               else '1900'
-             end as season
-     from f1_data.v_f1_seasons_race_dates r
-          ,future_races x
-     where r.season = p_in_cur_year
-       and to_number(r.round) in (select max(to_number(rd.round)) from f1_data.v_f1_seasons_race_dates rd
-                                  where rd.season  = r.season)
-  );
-
+  if lv_races = 0 then
+    lv_season := to_char(to_number(to_char(current_date,'RRRR') - 1));
+  else
+  
+    with future_races as -- We need to handle between seasons where there are no races
+      (
+        select /*+ MATERIALIZE */ count(vfu.season) as any_races
+        from f1_data.v_f1_upcoming_races vfu
+        where vfu.season = substr(to_char(trunc(sysdate,'YEAR')),1,4)  --Fix to YEAR and substr(1,4) to garantee that we only get the YEAR part
+      )
+      select season into lv_season -- Is current season finished yet?
+      from
+      (
+       select to_date(r.race_date,'RRRR-MM-DD') as race_date
+              ,case
+                 when (r.race_date < trunc(sysdate) and x.any_races = 0 and lv_races = 0 and to_char(to_number(to_char(current_date,'RRRR'))) = p_in_cur_year) then to_char(to_number(to_char(current_date,'RRRR'))) -- season finished but same year 
+                 when (r.race_date < trunc(sysdate) and x.any_races < 1) then to_char(to_number(to_char(current_date,'RRRR') - 1)) -- season finished but next years season has not yet started
+                 --when r.race_date < trunc(sysdate) then to_char(current_date,'RRRR')
+                 when (r.race_date > trunc(sysdate) and x.any_races > 0) then to_char(current_date,'RRRR')
+                 when (r.race_date > trunc(sysdate) and x.any_races = 0) then to_char(to_number(to_char(current_date,'RRRR') - 1))
+                 else '1900'
+               end as season
+       from f1_data.v_f1_seasons_race_dates r
+            ,future_races x
+       where r.season = p_in_cur_year
+         and to_number(r.round) in (select max(to_number(rd.round)) from f1_data.v_f1_seasons_race_dates rd
+                                    where rd.season  = r.season)
+    );
+    
+  end if;
+  
   return lv_season;
 
 end get_cur_f1_season;
+/
