8a205e638b9c3d49e2018e961fa4e002200aee67
Ulf Hellström <oraminute@gmail.com>
2024-11-25 15:27:42 +0100

    Updated package script with define off due to offset in pkb.

--- sql/lb_install_f1_logik.sql
+++ sql/lb_install_f1_logik.sql
@@ -3,7 +3,9 @@ REM Run this script connected as F1_DATA from the sql folder
 REM
 cd ../F1_LOGIK
 pwd
+set define off
 lb update -changelog-file controller.xml
 lb tag -tag version_2.0
 cd ../sql
 pwd
+set define on
