33dc1bf6a8bedc0eb08c9162d3a6c18b4389ae57
Ulf Hellström <oraminute@gmail.com>
2025-01-08 12:02:39 +0100

    Updated demo queries

--- sql/demo/demo_queries.sql
+++ sql/demo/demo_queries.sql
@@ -319,36 +319,25 @@ select season
 --
 -- Find all swedish formula 1 drivers and the years they where active
 -- 
-select *
-from
-(
-with get_drivers as (
-select fd.driverid
-      ,fd.permanentnumber
-      ,fd.code
-      ,givenname
-      ,fd.familyname
-  from v_f1_drivers fd
- where fd.nationality = 'Swedish'
- )
- select get_drivers.driverid
- ,get_drivers.permanentnumber
- ,get_drivers.code
- ,get_drivers.givenname
- ,get_drivers.familyname
- ,count(fr.racedate) as number_of_starts 
- ,to_char(min(fr.racedate),'RRRR-MM-DD') as starting_race
- ,to_char(max(fr.racedate),'RRRR-MM-DD') as end_race
- ,listagg(distinct(extract(YEAR FROM fr.racedate)),',') within group(order by extract(YEAR FROM fr.racedate)) as years_active
- from v_f1_results fr
- inner join get_drivers
- on fr.driverid = get_drivers.driverid
- group by get_drivers.driverid 
-   ,get_drivers.permanentnumber
-   ,get_drivers.code
-   ,get_drivers.givenname
-   ,get_drivers.familyname
-) order by starting_race;
+
+select d.driverid,
+       d.permanentnumber,
+       d.code,
+       d.givenname,
+       d.familyname,
+       count(r.racedate) as number_of_starts,
+       min(r.racedate) as starting_race,
+       max(r.racedate) as end_race,
+       listagg(distinct(extract(YEAR FROM r.racedate)),',') within group(order by extract(YEAR FROM r.racedate)) as years_active
+from v_f1_drivers d
+inner join v_f1_results r on d.driverid = r.driverid
+where d.nationality = 'Swedish'
+group by d.driverid,
+         d.permanentnumber,
+         d.code,
+         d.givenname,
+         d.familyname
+order by starting_race;
 
 REM
 REM Other interesting queries
