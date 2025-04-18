866ba1a5addd612258634a8e60fbc778a3871fd5
Ulf Hellström <oraminute@gmail.com>
2025-04-18 11:36:06 +0200

    Fixed v_f1_driverstancings_view

--- F1_ACCESS/view/v_f1_driverstandings_view.sql
+++ F1_ACCESS/view/v_f1_driverstandings_view.sql
@@ -1,4 +1,20 @@
 CREATE OR REPLACE FORCE EDITIONABLE VIEW "V_F1_DRIVERSTANDINGS" 
  ( "SEASON", "RACE", "POSITION", "POSITIONTEXT", "POINTS", "WINS", "DRIVERID", "CONSTRUCTORID"
   )  AS 
-  select "SEASON","RACE","POSITION","POSITIONTEXT","POINTS","WINS","DRIVERID","CONSTRUCTORID" from F1_DATA.F1_DRIVERSTANDINGS
+  select fds.season,
+       fds.race,
+       fds.position,
+       fds.positiontext,
+       fds.points,
+       fds.wins,
+       fds.driverid,
+       (case 
+           when fds.constructorid is null then 
+               (select vfr.constructorid 
+                from v_f1_results vfr
+                where vfr.season = fds.season
+                  and vfr.driverid = fds.driverid
+                  and vfr.race = fds.race)
+           else fds.constructorid
+        end) as constructorid
+from f1_data.f1_driverstandings fds
