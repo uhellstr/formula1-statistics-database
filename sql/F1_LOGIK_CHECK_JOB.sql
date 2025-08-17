0f9974ee6f13ae94ffa3208fb3d272c0dd2f5910
Ulf Hellström <oraminute@gmail.com>
2025-08-17 10:42:58 +0200

    updated sql script.

--- sql/F1_LOGIK_CHECK_JOB.sql
+++ sql/F1_LOGIK_CHECK_JOB.sql
@@ -1,5 +1,7 @@
 --
 -- Run as F1_LOGIK to check wether or not job succeeded
 --
-select * from USER_SCHEDULER_JOB_RUN_DETAILS
+select log_id,log_date, owner, job_name, status, error#, REQ_START_DATE
+actual_start_date, run_duration,additional_info 
+from USER_SCHEDULER_JOB_RUN_DETAILS
 order by log_date desc;
